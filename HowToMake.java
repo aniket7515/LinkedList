a80a246fcdaf1681d21570030680fd3b78829fcc
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2021-12-28 22:39:11 +0530

    Add files via upload

---
+++ HowToMake.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class HowToMake {
+    // Make a LinkedList
+    // A LinkedList contains a Node which contains a data and next
+    // data is a integer
+    // next is pointing to another node its type is Node
+    public static class Node{
+        int data;
+        Node next;
+
+    }
+    // Now as we created a Node class now we will create a linked list class which contains head , tail and size
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}

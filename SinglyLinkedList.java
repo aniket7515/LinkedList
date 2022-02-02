fd9bd2ae851a01e413afa93fffbaae3d411ccd7c
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-02-02 22:42:16 +0530

    Create SinglyLinkedList.java

---
+++ SinglyLinkedList.java
@@ -0,0 +1,68 @@
+
+public class SinglyLinkedList {
+    private ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void display(){
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.data+"-->");
+            current=current.next;
+        }
+        System.out.println("null");
+    }
+    public void length(){
+        if(head==null){
+            System.out.println(0);
+        }
+
+        ListNode temp=head;
+        int len=0;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        System.out.println(len);
+    }
+    // Insert a Node at beginning of singly LInkedlost
+
+    public void insertFirst(int value){
+        ListNode newNode=new ListNode(value);
+        newNode.next=head;
+        head=newNode;
+
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList ssl=new SinglyLinkedList();
+
+        ssl.head=new ListNode(10);
+        ListNode second=new ListNode(1);
+        ListNode third=new ListNode(8);
+        ListNode fourth=new ListNode(11);
+
+        // Now we will connect them together to form a chain
+
+        ssl.head.next=second;// 10-->1
+        second.next=third; // 10-->1-->8
+        third.next=fourth;// 10-->1-->8-->11-->null
+
+        ssl.display();
+        ssl.length();
+        ssl.insertFirst(23);
+        ssl.length();
+        ssl.display();
+
+
+    }
+}
+
+
